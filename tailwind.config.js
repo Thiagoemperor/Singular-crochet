29ea1bcf9f4ac65be80ce3cb2307831388a1ab6f
Thiagoemperor <76501540+Thiagoemperor@users.noreply.github.com>
2026-03-19 17:19:54 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#FFF8F0",
+        rose: "#8a5a6e"
+      }
+    }
+  },
+  plugins: []
+};
