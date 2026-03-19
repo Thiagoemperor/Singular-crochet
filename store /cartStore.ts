ada85af1b9548f1cff35b6e3351b55e8e7ce4d47
Thiagoemperor <76501540+Thiagoemperor@users.noreply.github.com>
2026-03-19 17:23:27 +0000

    Update cartStore.ts

--- store /cartStore.ts
+++ store /cartStore.ts
@@ -1,1 +1,9 @@
+import { create } from "zustand";
 
+export const useCartStore = create((set) => ({
+  items: [],
+  addToCart: (product: any) =>
+    set((state: any) => ({
+      items: [...state.items, product]
+    }))
+}));
