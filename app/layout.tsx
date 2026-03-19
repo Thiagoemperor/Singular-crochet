e8246c767263ab9c2a085e9f28363d5cd0032f8e
Thiagoemperor <76501540+Thiagoemperor@users.noreply.github.com>
2026-03-19 17:22:34 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Singular Crochet",
+  description: "Piezas únicas tejidas a mano"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
