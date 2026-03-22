e2befe940bafffa6d23f2b02c2196c3aa31f7275
Thiagoemperor <76501540+Thiagoemperor@users.noreply.github.com>
2026-03-22 16:43:56 +0000

    Update CartIcon.tsx

--- components/components/cart/CartIcon.tsx
+++ components/components/cart/CartIcon.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import { useState } from "react";
 import { useCartStore } from "../../store/cartStore";
+import CartDrawer from "./CartDrawer";
 
 export default function CartIcon() {
   const items = useCartStore((state: any) => state.items);
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="relative">
-      <span className="text-2xl">🛒</span>
+    <>
+      <div className="relative cursor-pointer" onClick={() => setOpen(true)}>
+        <span className="text-2xl">🛒</span>
 
-      {items.length > 0 && (
-        <span className="absolute -top-2 -right-2 bg-[#8a5a6e] text-white text-xs px-2 py-1 rounded-full">
-          {items.length}
-        </span>
-      )}
-    </div>
+        {items.length > 0 && (
+          <span className="absolute -top-2 -right-2 bg-[#8a5a6e] text-white text-xs px-2 py-1 rounded-full">
+            {items.length}
+          </span>
+        )}
+      </div>
+
+      <CartDrawer open={open} setOpen={setOpen} />
+    </>
   );
 }
