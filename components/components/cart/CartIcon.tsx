6617dd16f79386e52dcba597381ef096b87fa579
Thiagoemperor <76501540+Thiagoemperor@users.noreply.github.com>
2026-03-23 01:43:02 +0000

    Update CartIcon.tsx

--- components/components/cart/CartIcon.tsx
+++ components/components/cart/CartIcon.tsx
@@ -1,32 +1,27 @@
 "use client";
 
 import { useState } from "react";
-import { useCartStore } from "../../store/cartStore";
-import CartDrawer from "./CartDrawer";
 
 export default function CartIcon() {
-  const items = useCartStore((state: any) => state.items);
   const [open, setOpen] = useState(false);
 
   return (
     <>
       <div
-        className="relative cursor-pointer"
+        className="text-3xl cursor-pointer"
         onClick={() => {
-          console.log("click carrito");
+          console.log("CLICK FUNCIONA");
           setOpen(true);
         }}
       >
-        <span className="text-2xl">🛒</span>
-
-        {items.length > 0 && (
-          <span className="absolute -top-2 -right-2 bg-[#8a5a6e] text-white text-xs px-2 py-1 rounded-full">
-            {items.length}
-          </span>
-        )}
+        🛒
       </div>
 
-      {open && <CartDrawer open={open} setOpen={setOpen} />}
+      {open && (
+        <div className="fixed top-0 right-0 w-80 h-full bg-red-500 z-50">
+          <p className="text-white p-4">SE ABRIÓ 🔥</p>
+        </div>
+      )}
     </>
   );
 }
