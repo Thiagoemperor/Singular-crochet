096057737e609ce25f2c839a4109533586d5c00c
Thiagoemperor <76501540+Thiagoemperor@users.noreply.github.com>
2026-03-23 01:59:15 +0000

    Update CartIcon.tsx

--- components/components/cart/CartIcon.tsx
+++ components/components/cart/CartIcon.tsx
@@ -10,7 +10,7 @@ export default function CartIcon() {
       <div
         className="text-3xl cursor-pointer"
         onClick={() => {
-          console.log("CLICK FUNCIONA");
+          alert("click anda");
           setOpen(true);
         }}
       >
@@ -18,7 +18,7 @@ export default function CartIcon() {
       </div>
 
       {open && (
-        <div className="fixed top-0 right-0 w-80 h-full bg-red-500 z-50">
+        <div className="fixed top-0 right-0 w-80 h-full bg-red-500 z-[9999]">
           <p className="text-white p-4">SE ABRIÓ 🔥</p>
         </div>
       )}
